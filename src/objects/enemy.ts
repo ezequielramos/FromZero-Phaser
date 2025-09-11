0e7b2452e14db1ba2f3f564194ec2878d1a9d2ec
Ezequiel Ramos <ezequielmr94@gmail.com>
2025-09-11 13:52:19 -0300

    adding a better moviment to enemy

--- src/objects/enemy.ts
+++ src/objects/enemy.ts
@@ -1,31 +1,59 @@
 export class Enemy extends Phaser.Physics.Arcade.Image {
-    private speed: number;
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
         super(scene, x, y, texture);
         scene.add.existing(this);
         scene.physics.add.existing(this);
 
-        this.speed = -50;
-
 
         scene.tweens.add({
             targets: this,
             x: x + 200,
-            duration: 2000,
-            yoyo: true,
+            duration: 3000,
+            yoyo: false,
             repeat: -1,
+            repeatDelay: 500,
             ease: 'Linear',
         });
+
+        scene.tweens.add({
+            targets: this,
+            y: y + 600,
+            duration: 1500,
+            yoyo: true,
+            repeat: -1,
+            repeatDelay: 500,
+            ease: 'Sine.easeInOut',
+            onRepeat: () => {
+                scene.tweens.add({
+                    targets: this,
+                    angle: 0,
+                    duration: 0,
+                });
+
+
+                scene.time.delayedCall(1250, () => {
+                    scene.tweens.add({
+                        targets: this,
+                        angle: -180,
+                        duration: 500,
+                    });
+                });
+
+            }
+        });
+
+        scene.time.delayedCall(1250, () => {
+            scene.tweens.add({
+                targets: this,
+                angle: -180,
+                duration: 500,
+            });
+        });
+
     }
 
     update(time: number, delta: number) {
         super.update(time, delta);
-
-        this.y -= this.speed * (delta / 1000);
-
-        if (this.y > 1000) {
-            this.destroy();
-        }
     }
 }
